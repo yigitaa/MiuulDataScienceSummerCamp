6b2cd5e5ef2d0d11b65f0e0842247312defe4fa1
Yiğit Ahmet Arıkök <yigitarikok@outlook.com>
2022-11-04 17:23:39 +0300

    Update import.sql

--- classification/rfm-olist-kmeans-segmentation/import.sql
+++ classification/rfm-olist-kmeans-segmentation/import.sql
@@ -1,35 +1,35 @@
 COPY customers
-FROM 'X:\Miuul\pythonProject\OLIST\olist_customers_dataset.csv'
+FROM '...\OLIST\olist_customers_dataset.csv'
 WITH (FORMAT CSV, HEADER);
 
 COPY sellers
-FROM 'X:\Miuul\pythonProject\OLIST\olist_sellers_dataset.csv'
+FROM '...\OLIST\olist_sellers_dataset.csv'
 WITH (FORMAT CSV, HEADER);
 
 COPY products
-FROM 'X:\Miuul\pythonProject\OLIST\olist_products_dataset.csv'
+FROM '...\OLIST\olist_products_dataset.csv'
 WITH (FORMAT CSV, HEADER);
 
 COPY orders
-FROM 'X:\Miuul\pythonProject\OLIST\olist_orders_dataset.csv'
+FROM '...\OLIST\olist_orders_dataset.csv'
 WITH (FORMAT CSV, HEADER);
 
 COPY order_payments
-FROM 'X:\Miuul\pythonProject\OLIST\olist_order_payments_dataset.csv'
+FROM '...\OLIST\olist_order_payments_dataset.csv'
 WITH (FORMAT CSV, HEADER);
 
 COPY order_reviews
-FROM 'X:\Miuul\pythonProject\OLIST\olist_order_reviews_dataset.csv'
+FROM '...\OLIST\olist_order_reviews_dataset.csv'
 WITH (FORMAT CSV, HEADER);
 
 COPY order_items
-FROM 'X:\Miuul\pythonProject\OLIST\olist_order_items_dataset.csv'
+FROM '...\OLIST\olist_order_items_dataset.csv'
 WITH (FORMAT CSV, HEADER);
 
 COPY product_translation
-FROM 'X:\Miuul\pythonProject\OLIST\product_category_name_translation.csv'
+FROM '...\OLIST\product_category_name_translation.csv'
 WITH (FORMAT CSV, HEADER);
 
 COPY geo_location
-FROM 'X:\Miuul\pythonProject\OLIST\olist_geolocation_dataset.csv'
+FROM '...\OLIST\olist_geolocation_dataset.csv'
 WITH (FORMAT CSV, HEADER);
